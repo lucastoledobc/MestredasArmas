368e3d5cea9560af00bcdbd5931a1a9604e576cf
ToledoLBC <lucastoledobm@gmail.com>
2025-11-11 15:16:12 -0300

    def

--- MdAMenu.py
+++ MdAMenu.py
@@ -1,8 +1,15 @@
 from PPlay.mouse import *
 from PPlay.window import *
+from PPlay.sprite import *
 
-def running_menu(screen,room,mouse):
+
+def running_menu(screen,room,mouse,titulo,bjoga,bdifc,brank,bsair):
     screen.set_background_color([0, 0, 0])
+    titulo.draw()
+    bjoga.draw()
+    bdifc.draw()
+    brank.draw()
+    bsair.draw()
 
-    if mouse.is_button_pressed(1):
+    if mouse.is_button_pressed(1) and mouse.is_over_object(bjoga) :
         room[0] = "Jogo"
--- MdAmain.py
+++ MdAmain.py
@@ -22,10 +22,25 @@ running = [True]
 while running[0]:
     if room[0] == "Menu":
         if entrar:
+            # título e botões
+            titulo = Sprite("MdASprites/Menu/titulo.png")
+            titulo.set_position(int(screen.width/2)-titulo.width/2, 30)
+
+            bjoga = Sprite("MdASprites/Menu/bjoga.png")
+            bjoga.set_position(int(screen.width/2)-bjoga.width/2, 300)
+            bdifc = Sprite("MdASprites/Menu/bdifc.png")
+            bdifc.set_position(int(screen.width/2)-bdifc.width/2, 350)
+            brank = Sprite("MdASprites/Menu/brank.png")
+            brank.set_position(int(screen.width/2)-brank.width/2, 400)
+            bsair = Sprite("MdASprites/Menu/bsair.png")
+            bsair.set_position(int(screen.width/2)-bsair.width/2, 450)
+
+            # mouse e teclado
             mouse = screen.get_mouse()
+            
             entrar = False
 
-        running_menu(screen,room,mouse)
+        running_menu(screen,room,mouse,titulo,bjoga,bdifc,brank,bsair)
 
         if room[0] != "Menu":
             entrar = True
